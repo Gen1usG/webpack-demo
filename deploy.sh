beecf2a6bd0e4ea936d3a0818d1ce2024370d8f8
Gen1usG <812487021@qq.com>
2019-12-17 02:33:24 +0800

    c

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 yarn build &&
 git branch gh-page &&
 git checkout gh-page &&
-rm -rf *.html *.js *.css *.png && && 
+rm -rf *.html *.js *.css *.png && 
 mv dist/* ./ && 
 rm dist && 
 git add . && 
